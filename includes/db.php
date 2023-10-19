490de980fe2fb85564cb6abc8ea47caae9ca96a2
Emmanuel <55505148+Emmanuel-XD@users.noreply.github.com>
2023-10-19 12:03:47 -0600

    conexion a db

--- includes/db.php
+++ includes/db.php
@@ -1,12 +1,13 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "control_escolar";
+$host = "czu.h.filess.io";
+$user = "controlEscolar_valuableif";
+$password = "72659f651655f158ad383189884879d156b523bf";
+$database = "controlEscolar_valuableif";
+$puerto = "3305";
 
 
-$conexion = mysqli_connect($host, $user, $password, $database);
+$conexion = mysqli_connect($host, $user, $password, $database, $puerto);
 if (!$conexion) {
     echo "No se realizo la conexion a la basa de datos, el error fue:" .
         mysqli_connect_error();
