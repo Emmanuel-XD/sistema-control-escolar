b08249da6d4129a53a50e66a57f5e5c155cab823
Emmanuel <55505148+Emmanuel-XD@users.noreply.github.com>
2023-10-19 16:50:59 -0600

    Add files via upload

--- includes/pdf_prestamo.php
+++ includes/pdf_prestamo.php
@@ -34,8 +34,8 @@ class PDF extends FPDF
         $this->setY(10);
         $this->SetX(75);
 
-       // $this->image('./img/logo.jpg', 5, 4, 35);  // X, Y, Tamaño
-       // $this->image('./img/sistemas.png', 170, 0, 50);  // X, Y, Tamaño
+        // $this->image('./img/logo.jpg', 5, 4, 35);  // X, Y, Tamaño
+        // $this->image('./img/sistemas.png', 170, 0, 50);  // X, Y, Tamaño
 
         $this->Cell(70, 10, 'NOMBRE DE INSTITUCION', 0, 1, 'C');
 
@@ -43,7 +43,7 @@ class PDF extends FPDF
         $this->setY(20);
         $this->setX(80);
 
-        $this->Cell(60, 4, 'AREASOLICITADA', 0, 1, 'C');
+        //$this->Cell(60, 4, 'CLASE SOLICITADA', 0, 1, 'C');
 
         $this->SetFont('Arial', 'B', 12);
         $this->setY(35);
@@ -55,6 +55,24 @@ class PDF extends FPDF
         $this->SetY(50);
         $this->SetX(10);
 
+        extract($_GET);
+        include "db.php";
+        $consulta = "SELECT pr.id, pr.id_profesor, pr.id_materia, pr.id_material, pr.fecha_slt, pr.fecha_fin,pr.fecha_registrado,
+        pr.hora_in, pr.hora_fin, pr.cant, pr.status, pr.fecha_registrado,p.nombres, p.apellidos, m.materia, i.descripcion, i.unidad, 
+        g.descripcion AS grado FROM prestamos pr INNER JOIN profesores p ON pr.id_profesor = p.id INNER JOIN materias m ON pr.id_materia = m.id 
+        INNER JOIN inventario i ON pr.id_material = i.id INNER JOIN grados g ON m.id_grado = g.id WHERE pr.id = $id";
+        $resultado = mysqli_query($conexion, $consulta);
+        $fila = mysqli_fetch_assoc($resultado);
+        $this->Cell(60, 4, 'Nombre del Solicitante: ' . utf8_decode($fila['nombres'] . ' ' . $fila['apellidos']), 0, 1, 'L');
+
+        $this->setY(57);
+        $this->setX(10);
+        $this->Cell(60, 4, 'Clase Solicitada: ' . utf8_decode($fila['materia'] . ' - ' . $fila['grado']), 0, 1, 'L');
+
+
+        $this->setY(65);
+        $this->setX(10);
+        $this->Cell(60, 4, 'Fecha Registrada: ' . utf8_decode($fila['fecha_registrado']), 0, 1, 'L');
 
         /* $this->setY(70);
         $this->setX(50);
@@ -63,17 +81,20 @@ class PDF extends FPDF
         // Salto de línea
         $this->SetFont('Arial', 'B', 10);
         $this->Ln(15);
-        $this->SetX(20);
+        $this->SetX(10);
 
 
 
         $this->SetFont('Arial', 'B', 8);
-        $this->SetX(15);
+        $this->SetX(12);
 
-        $this->Cell(30, 10, 'N. Del. Material', 1, 0, 'C', 0);
-        $this->Cell(17, 10, 'Cantidad', 1, 0, 'C', 0);
-        $this->Cell(100, 10, 'Observaciones', 1, 0, 'C', 0);
-        $this->Cell(30, 10, 'Fecha', 1, 1, 'C', 0);
+        $this->Cell(43, 10, 'Material Solicitado', 0, 0, 'L', 0);
+        $this->Cell(12, 10, 'Cant', 0, 0, 'L', 0);
+        $this->Cell(25, 10, 'Fecha Solicitada', 0, 0, 'L', 0);
+        $this->Cell(25, 10, 'Fecha Devuelta', 0, 0, 'L', 0);
+        $this->Cell(25, 10, 'Hora Inicio', 0, 0, 'L', 0);
+        $this->Cell(20, 10, 'Hora Fin', 0, 0, 'L', 0);
+        $this->Cell(30, 10, 'Estado', 0,1, 'L', 0);
     }
 
     // Pie de página
@@ -90,13 +111,13 @@ class PDF extends FPDF
         $this->Ln(20);
         $this->setY(-80);
         $this->setX(-224);
-        $this->Cell(60, 0, utf8_decode('Nota:'), 0, 1, 'C');
+        $this->Cell(60, 0, utf8_decode('NOTA:'), 0, 1, 'C');
 
         $this->SetFont('Helvetica', '', 10);
         $this->Ln(20);
         $this->setY(-75);
         $this->setX(10);
-        $this->MultiCell($this->GetPageWidth() - 20, 5, utf8_decode('El alumno tiene 5 días máximos desde que pide el material, para que lo devuelva.En caso de extravío o daño por mal uso que haga el solicitante del material y equipo será responsabilidad del mismo y deberá ser repuesto por un máximo de 6 días hábiles.
+        $this->MultiCell($this->GetPageWidth() - 20, 5, utf8_decode('El solicitante tiene 5 días máximos desde que pide el material, para que lo devuelva. En caso de extravío o daño por mal uso que haga el solicitante del material y equipo será responsabilidad del mismo y deberá ser repuesto por un máximo de 6 días hábiles.
         '), 0, 'J');
 
         $this->SetFont('Helvetica', 'B', 7);
@@ -115,7 +136,7 @@ class PDF extends FPDF
         $this->Ln(20);
         $this->setY(-20);
         $this->setX(12);
-        $this->Cell(60, 0, utf8_decode('De quien recibe'), 0, 1, 'C');
+        $this->Cell(60, 0, utf8_decode('Del Solicitante'), 0, 1, 'C');
         //Otra firma
         $this->SetFont('Helvetica', 'B', 7);
         $this->Ln(20);
@@ -146,8 +167,7 @@ $id = $_GET['id'];
 $consulta = "SELECT pr.id, pr.id_profesor, pr.id_materia, pr.id_material, pr.fecha_slt, pr.fecha_fin,
 pr.hora_in, pr.hora_fin, pr.cant, pr.status, pr.fecha_registrado,p.nombres, p.apellidos, m.materia, i.descripcion, i.unidad 
 FROM prestamos pr INNER JOIN profesores p ON pr.id_profesor = p.id INNER JOIN materias m ON pr.id_materia = m.id 
-INNER JOIN inventario i ON pr.id_material = i.id
-WHERE pr.id = $id";
+INNER JOIN inventario i ON pr.id_material = i.id WHERE pr.id = $id";
 $resultado = mysqli_query($conexion, $consulta);
 
 
@@ -159,12 +179,15 @@ $pdf->SetFont('Arial', '', 0);
 //$pdf->SetWidths(array(10, 30, 27, 27, 20, 20, 20, 20, 22));
 while ($row = $resultado->fetch_assoc()) {
 
-    $pdf->SetX(15);
+    $pdf->SetX(12);
 
-    $pdf->Cell(30, 10, $row['descripcion'], 1, 0, 'C', 0);
-    $pdf->Cell(17, 10, $row['cant'], 1, 0, 'C', 0);
-    $pdf->Cell(100, 10, $row['id_material'], 1, 0, 'L', 0);
-    $pdf->Cell(30, 10, $row['status'], 1, 1, 'C', 0);
+    $pdf->Cell(43, 10, $row['descripcion'], 0, 0, 'L', 0);
+    $pdf->Cell(12, 10, $row['cant'], 0, 0, 'L', 0);
+    $pdf->Cell(25, 10, $row['fecha_slt'], 0, 0, 'L', 0);
+    $pdf->Cell(25, 10, $row['fecha_fin'], 0, 0, 'L', 0);
+    $pdf->Cell(25, 10, $row['hora_in'], 0, 0, 'L', 0);
+    $pdf->Cell(20, 10, $row['hora_fin'], 0, 0, 'L', 0);
+    $pdf->Cell(30, 10, $row['status'], 0, 1, 'L', 0);
 }
 
 
