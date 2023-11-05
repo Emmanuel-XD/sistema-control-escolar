f0cb285e9a97c365696bd9aa23342f6ffd7c897a
Emmanuel <55505148+Emmanuel-XD@users.noreply.github.com>
2023-11-05 09:40:32 -0600

    grupos agregados

---
+++ views/form_grupo.php
@@ -0,0 +1,58 @@
+<div class="modal fade" id="grupo" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header bg-primary text-white">
+                <h3 class="modal-title" id="exampleModalLabel">Agregar nuevo grupo</h3>
+                <button type="button" class="btn btn-primary" data-dismiss="modal">
+                    <i class="fa fa-times" aria-hidden="true"></i></button>
+            </div>
+            <div class="modal-body">
+
+                <form id="addForm">
+
+
+                    <div class="form-group">
+                        <label for="nombre" class="form-label">Grupo</label>
+                        <input type="text" id="grupo" name="grupo" class="form-control" required>
+                    </div>
+
+
+                    <br>
+
+                    <input type="hidden" name="accion" value="insert_grupo">
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary" id="register" name="registrar">Guardar</button>
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
+                    </div>
+
+            </div>
+
+            </form>
+        </div>
+    </div>
+</div>
+<script>
+    $(document).ready(function() {
+        $('#addForm').submit(function(e) {
+            e.preventDefault();
+            var formData = $(this).serialize();
+            $.ajax({
+                url: '../includes/functions.php',
+                type: 'POST',
+                data: formData,
+                dataType: 'json',
+                success: function(response) {
+                    if (response.status === 'success') {
+                        alert('Éxito: Los datos se guardaron correctamente');
+                        window.location = "grupos.php";
+                    } else {
+                        alert('Error: Ocurrió un error inesperado');
+                    }
+                },
+                error: function(xhr, status, error) {
+                    alert('Error: Ocurrió un error inesperado');
+                }
+            });
+        });
+    });
+</script>
---
+++ views/grupos.php
@@ -0,0 +1,87 @@
+<?php include "../includes/header.php"; ?>
+
+
+<body id="page-top">
+
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+
+        <!-- DataTales Example -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Lista de Grupos</h6>
+                <br>
+
+                <button type="button" class="btn btn-success" data-toggle="modal" data-target="#grupo">
+                    <span class="glyphicon glyphicon-plus"></span> Agregar <i class="fa fa-plus"></i> </a></button>
+            </div>
+            <?php include "form_grupo.php"; ?>
+
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>Grupo</th>
+                                <th>Fecha_registro</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <?php
+                            require_once("../includes/db.php");
+                            $result = mysqli_query($conexion, "SELECT * FROM grupos ");
+                            while ($fila = mysqli_fetch_assoc($result)) :
+
+                            ?>
+                                <tr>
+                                    <td><?php echo $fila['grupo']; ?></td>
+                                    <td><?php echo $fila['fecha']; ?></td>
+
+                                    <td>
+                                        <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#editar<?php echo $fila['id']; ?>">
+                                            <i class="fa fa-edit "></i>
+                                        </button>
+                                        <a href="../includes/.php?id=<?php echo $fila['id'] ?>" class="btn btn-danger btn-del">
+                                            <i class="fa fa-trash "></i></a>
+                                    </td>
+                                </tr>
+                                <?php include "editar_grupo.php"; ?>
+                            <?php endwhile; ?>
+                        </tbody>
+                    </table>
+
+
+
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container-fluid -->
+
+    </div>
+    <!-- End of Main Content -->
+
+
+
+
+
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+
+
+
+</body>
+
+
+<?php include "../includes/footer.php"; ?>
+
+</html>
