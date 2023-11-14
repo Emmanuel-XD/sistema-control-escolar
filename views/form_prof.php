9129cf88aec06f8705f8b2e35a3a9ee86e56796d
Emmanuel <55505148+Emmanuel-XD@users.noreply.github.com>
2023-11-13 21:39:26 -0600

    profesores modificado

--- views/form_prof.php
+++ views/form_prof.php
@@ -88,7 +88,23 @@
                             </div>
                         </div>
                     </div>
+                    <div class="form-group">
+                        <label for="id_usuario">Usuario Asociado</label><br>
+                        <select name="id_usuario" id="id_usuario" class="form-control" required>
+                            <option value="">Selecciona una opcion</option>
+                            <?php
 
+                            include("db.php");
+
+                            $sql = "SELECT * FROM users WHERE id_rol = 2 ";
+                            $resultado = mysqli_query($conexion, $sql);
+                            while ($consulta = mysqli_fetch_array($resultado)) {
+                                echo '<option value="' . $consulta['id'] . '">' . $consulta['usuario'] . '</option>';
+                            }
+
+                            ?>
+                        </select>
+                    </div>
                     <br>
                     <input type="hidden" name="accion" value="insert_prof">
                     <div class="modal-footer">
@@ -114,14 +130,33 @@
                 dataType: 'json',
                 success: function(response) {
                     if (response.status === 'success') {
-                        alert('Éxito: Los datos se guardaron correctamente');
-                        window.location = "profesores.php";
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Datos Guardados',
+                            text: 'Los datos se guardaron correctamente'
+                        }).then(function() {
+                            window.location = "profesores.php";
+                        });
+                    } else if (response.status === 'user') {
+                        Swal.fire({
+                            icon: 'info',
+                            title: 'Usuario No Disponible',
+                            text: 'El usuario seleccionado ya está registrado como profesor, intente con otro o registre uno nuevo.'
+                        });
                     } else {
-                        alert('Error: Ocurrió un error inesperado');
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Error',
+                            text: 'Ocurrió un error inesperado'
+                        });
                     }
                 },
                 error: function(xhr, status, error) {
-                    alert('Error: Ocurrió un error inesperado');
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Error',
+                        text: 'Ocurrió un error inesperado'
+                    });
                 }
             });
         });
