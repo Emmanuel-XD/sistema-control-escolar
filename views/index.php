d3a48e59d60541af275edcea3b64693001b9c888
Emmanuel <55505148+Emmanuel-XD@users.noreply.github.com>
2023-11-14 00:13:23 -0600

    index modificado

--- views/index.php
+++ views/index.php
@@ -103,7 +103,7 @@ include "../includes/header.php";
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                Pending Requests</div>
+                                Usuarios</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">
                                 <?php
                                 include "../includes/db.php";
