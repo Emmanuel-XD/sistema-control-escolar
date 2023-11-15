f31caaddc94855948f2e0eaa4dd75b2bcbd87407
Emmanuel <55505148+Emmanuel-XD@users.noreply.github.com>
2023-11-15 16:58:08 -0600

    Funcion de selector agregado

---
+++ views/consult_history.php
@@ -0,0 +1,112 @@
+<?php
+error_reporting(0);
+session_start();
+$usuario = $_SESSION['usuario'];
+$permiso = $_SESSION['type'];
+if ($usuario == null || $usuario == ''  && $permiso == null || $permiso == '') {
+
+    echo "<script language='JavaScript'>
+    alert('Error: Debes iniciar sesion primero ');
+    location.assign('../includes/sesion/login.php');
+    </script>";
+
+    die();
+}
+include "../includes/db.php";
+
+
+$startDate = $_POST['start'];
+$endDate = $_POST['end'];
+
+$sql = "SELECT p.id, p.total, p.pago, p.fecha,p.beca, a.matricula, a.nombre, a.apellido, g.descripcion, c.cargo, c.monto
+FROM pagos p INNER JOIN alumnos a ON p.id_alumno = a.id INNER JOIN grados g ON p.id_grado = g.id INNER JOIN cargos c 
+ON p.id_cargo = c.id";
+
+if (!empty($startDate) && !empty($endDate)) {
+    $sql .= " WHERE DATE(p.fecha) BETWEEN '$startDate' AND '$endDate'";
+}
+
+$result = mysqli_query($conexion, $sql);
+
+$output = '';
+$total = 0;
+
+while ($fila = mysqli_fetch_array($result)) {
+    $output .= '<tr>
+    <td>' . $fila['nombre'] . ' ' . $fila['apellido'] . '</td>
+    <td>' . $fila['descripcion'] . '</td>
+    <td>' . $fila['cargo'] . '</td>
+    <td>' . $fila['beca'] . '%' . '</td>
+    <td>' . '$' . $fila['pago'] . '</td>
+    <td>' . $fila['fecha'] . '</td>
+        <td>';
+
+    if ($_SESSION["type"] == 1) {
+        $output .= ' <a href="../includes/recibo.php?id=' . $fila['id'] . '" target="_blank" class="btn btn-outline-danger">
+        <i class="fa fa-file "></i></a>
+                    <a href="../includes/eliminar_pagina.php?id=' . $fila['id'] . '" class="btn btn-danger btn-del">
+                        <i class="fa fa-trash"></i>
+                    </a>';
+    }
+
+    $output .= '</td>
+    </tr>';
+
+    $total += $fila['pago'];
+}
+
+$totalOutput = '';
+if ($total != 0) {
+    $totalOutput = '<tr>
+                        <td class="text-right"><strong>Total de Pagos:</strong></td>
+                        <td><strong>$' . $total . '</strong></td>
+                    </tr>';
+}
+
+if (mysqli_num_rows($result) == 0) {
+    $output .= '<tr>
+                    <td colspan="6" class="text-center">No se encontraron registros</td>
+                </tr>';
+
+    $output .= '<script>
+                    Swal.fire({
+                        icon: "info",
+                        title: "No se encontraron registros",
+                        text: "No hay resultados para mostrar",
+                    });
+                </script>';
+}
+
+$output .= $totalOutput;
+
+echo $output;
+?>
+<script>
+    $('.btn-del').on('click', function(e) {
+        e.preventDefault();
+        const href = $(this).attr('href')
+
+        Swal.fire({
+            title: 'Estas seguro de eliminar este registro?',
+            text: "¡No podrás revertir esto!!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, eliminar!',
+            cancelButtonText: 'Cancelar!',
+        }).then((result) => {
+            if (result.value) {
+                if (result.isConfirmed) {
+                    Swal.fire(
+                        'Eliminado!',
+                        'El registro fue eliminado.',
+                        'success'
+                    )
+                }
+
+                document.location.href = href;
+            }
+        })
+    })
+</script>
--- views/historial.php
+++ views/historial.php
@@ -12,8 +12,38 @@
                 <h6 class="m-0 font-weight-bold text-primary">Historial de Pagos</h6>
                 <br>
 
+                <form action="" method="POST" accept-charset="utf-8" id="filtro-form">
+
+                    <div class="row">
+
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="" class="form-label"><b> Del dia</b></label>
+                                <input type="date" name="star" id="star" class="form-control" required>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="" class="form-label"><b>Hasta el dia</b> </label>
+                                <input type="date" name="fin" id="fin" class="form-control" required>
+                            </div>
+                        </div>
+
+
+
+                        <div class="col-md-2">
+                            <div class="form-group">
+                                <label for="" class="form-label"><b>Filtrar</b></label><br>
+                                <button type="button" id="filtro" class="btn btn-primary"><i class="fa fa-search" aria-hidden="true"></i></button>
+                            </div>
+                        </div>
+
+                    </div>
+                </form>
 
                 <a href="pagos.php" class="btn btn-success">Nuevo Pago <i class="fa fa-plus"></i></a>
+                <button onclick="exportarExcel()" class="btn btn-primary blue">Exportar a Excel <i class="fas fa-download fa-sm text-white-50"></i></button>
             </div>
 
 
@@ -28,7 +58,10 @@
                                 <th>Beca %</th>
                                 <th>Total</th>
                                 <th>Fecha_registro</th>
-                                <th>Acciones</th>
+                                <?php if ($_SESSION["type"] == 1) { ?>
+                                    <th>Acciones</th>
+                                <?php }
+                                ?>
                             </tr>
                         </thead>
 
@@ -49,15 +82,17 @@
                                     <td><?php echo '$' . $fila['pago']; ?></td>
 
                                     <td><?php echo $fila['fecha']; ?></td>
+                                    <?php if ($_SESSION["type"] == 1) { ?>
+                                        <td>
 
-                                    <td>
+                                            <a href="../includes/recibo.php?id=<?php echo $fila['id'] ?>" target="_blank" class="btn btn-outline-danger">
+                                                <i class="fa fa-file "></i></a>
 
-                                        <a href="../includes/recibo.php?id=<?php echo $fila['id'] ?>" target="_blank" class="btn btn-outline-danger">
-                                            <i class="fa fa-file "></i></a>
-
-                                        <a href="../includes/eliminar_pago.php?id=<?php echo $fila['id'] ?>" class="btn btn-danger btn-del">
-                                            <i class="fa fa-trash "></i></a>
-                                    </td>
+                                            <a href="../includes/eliminar_pago.php?id=<?php echo $fila['id'] ?>" class="btn btn-danger btn-del">
+                                                <i class="fa fa-trash "></i></a>
+                                        </td>
+                                    <?php }
+                                    ?>
                                 </tr>
 
                             <?php endwhile; ?>
@@ -92,7 +127,51 @@
 
 
 </body>
+<script>
+    $('#filtro').click(function(e) {
+        e.preventDefault();
+        var startDate = $('#star').val();
+        var endDate = $('#fin').val();
 
+        if (!startDate || !endDate) {
+            Swal.fire({
+                icon: 'warning',
+                title: 'Fechas No Seleccionadas',
+                text: 'Por favor, selecciona un rango de fechas antes de filtrar.',
+            });
+            return;
+        }
+
+        var data = {
+            start: startDate,
+            end: endDate
+        };
+
+        $.ajax({
+            url: 'consult_history.php',
+            method: 'POST',
+            data: data,
+            success: function(response) {
+                $('#dataTable tbody').html(response);
+            }
+        });
+    });
+</script>
+<script>
+    var tabla = document.querySelector("#dataTable");
+    var dataTable = new DataTable(tabla);
+
+    function exportarExcel() {
+        // Seleccionar la tabla
+        var tabla = document.getElementById("dataTable");
+
+        // Convertir la tabla en un archivo de Excel
+        var libro = XLSX.utils.table_to_book(tabla);
+
+        // Descargar el archivo
+        XLSX.writeFile(libro, "REPORTE_HISTORIAL_PAGOS.xlsx");
+    }
+</script>
 
 <?php include "../includes/footer.php"; ?>
 
